653dfca67249f42edf9577293282e32979c5a63e
Izabella B.Lopes <izabella.bl@hotmail.com>
2021-09-21 17:32:13 -0300

    Exercicios de Metodos

--- src/Exercicios/Metodos/MetodosComNumeros.java
+++ src/Exercicios/Metodos/MetodosComNumeros.java
@@ -7,9 +7,17 @@ public class MetodosComNumeros {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        double num2;
+        boolean isValido = true;
+
         System.out.println("---------------------Bem vindo!-----------------");
         double num1 = EntradaDeValores("Digite o primeiro numero:");
-        double num2 = EntradaDeValores("Digite o segundo numero:");
+
+        do{
+            num2 = EntradaDeValores("Digite o segundo numero:");
+            isValido = Validar(num2);
+        }while (!isValido);
+
 
         System.out.println(" Resultado em soma : "+ Soma(num1,num2));
         System.out.println(" Resultado em  subtração : "+ Subtracao(num1,num2));
@@ -46,5 +54,13 @@ public class MetodosComNumeros {
 
         double resultado = numero1 / numero2;
         return resultado;
+    }
+
+    static boolean Validar(double num){
+        if(num == 0){
+            System.out.println("Erro,numero nao pode ser zero!");
+            return false;
+        }
+        return true;
     }
 }

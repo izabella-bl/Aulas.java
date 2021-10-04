deef4b4e7c52f198fb5d64d3680312a27291a0f5
Izabella B.Lopes <izabella.bl@hotmail.com>
2021-10-04 17:21:05 -0300

    Exercicio arrays numero

---
+++ src/Exercicios/Objeto/ATP19/Numero/Numero.java
@@ -0,0 +1,24 @@
+package Exercicios.Objeto.ATP19.Numero;
+
+public class Numero {
+    public static void main(String[] args) {
+        int[] num = new int[10];
+
+        num[0] = 10;
+        num[1] = 9;
+        num[2] = 8;
+        num[3] = 7;
+        num[4] = 6;
+        num[5] = 5;
+        num[6] = 4;
+        num[7] = 3;
+        num[8] = 2;
+        num[9] = 1;
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+
+        }
+
+    }
+}

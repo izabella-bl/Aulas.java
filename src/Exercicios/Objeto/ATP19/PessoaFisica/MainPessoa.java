6aadd1ecf5a2b795e765960b975f8cc2b05f4ded
Izabella B.Lopes <izabella.bl@hotmail.com>
2021-10-04 17:42:09 -0300

    Exercicios array pessoa fisica

---
+++ src/Exercicios/Objeto/ATP19/PessoaFisica/MainPessoa.java
@@ -0,0 +1,58 @@
+package Exercicios.Objeto.ATP19.PessoaFisica;
+
+public class MainPessoa {
+    public static void main(String[] args) {
+        Pessoa[] pessoa = new  Pessoa[5];
+
+        Pessoa p1 = new Pessoa();
+        p1.setCodigo(9292818);
+        p1.setNome("Maria");
+        p1.setSobrenome("Silva");
+        p1.setIdade(35);
+        p1.setRg(928392);
+        p1.setCpf("933.838.939-83");
+
+        Pessoa p2= new Pessoa();
+        p2.setCodigo(2938489);
+        p2.setNome("João");
+        p2.setSobrenome("Lima");
+        p2.setIdade(24);
+        p2.setRg(4945833);
+        p2.setCpf("573.958.757-73");
+
+        Pessoa p3= new Pessoa();
+        p3.setCodigo(1929494);
+        p3.setNome("Ana");
+        p3.setSobrenome("Paula");
+        p3.setIdade(17);
+        p3.setRg(4948224);
+        p3.setCpf("902.847.839-43");
+
+        Pessoa p4 = new Pessoa();
+        p4.setCodigo(9292818);
+        p4.setNome("Luana");
+        p4.setSobrenome("Oliveira");
+        p4.setIdade(30);
+        p4.setRg(3746223);
+        p4.setCpf("073.838.828-53");
+
+        Pessoa p5 = new Pessoa();
+        p5.setCodigo(9292818);
+        p5.setNome("Luciana");
+        p5.setSobrenome("Pereira");
+        p5.setIdade(25);
+        p5.setRg(838293);
+        p5.setCpf("553.848.838-92");
+
+        pessoa[0]=p1;
+        pessoa[1]=p2;
+        pessoa[2]=p3;
+        pessoa[3]=p4;
+        pessoa[4]=p5;
+
+        for (Pessoa p:pessoa) {
+            System.out.printf("\nNome completo: %s %s\nIdade: %d\nRG: %d\nCPF: %s\nCodigo: %d\n",p.getNome(),p.getSobrenome(),p.getIdade(),p.getRg(),p.getCpf(),p.getCodigo());
+        }
+
+    }
+}
---
+++ src/Exercicios/Objeto/ATP19/PessoaFisica/Pessoa.java
@@ -0,0 +1,58 @@
+package Exercicios.Objeto.ATP19.PessoaFisica;
+
+public class Pessoa {
+    private  long  codigo;
+    private String nome;
+    private  String sobrenome;
+    private int idade;
+    private  int rg;
+    private  String cpf;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getRg() {
+        return rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}

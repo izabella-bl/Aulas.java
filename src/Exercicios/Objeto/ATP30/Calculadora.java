01ac63c04e05681dd3b91f2e636dfd67b5ff069a
Izabella B.Lopes <izabella.bl@hotmail.com>
2021-10-25 13:29:03 -0300

    Menu com try - cath

--- src/Exercicios/Objeto/ATP30/Calculadora.java
+++ src/Exercicios/Objeto/ATP30/Calculadora.java
@@ -36,11 +36,23 @@ public class Calculadora {
         return (double) (this.numero1 / numero2);
     }
 
-    @Override
-    public String toString() {
-        return String.format("RESULTADO:\nSOMA: %d\n" +
-                "SUBTRAÇÃO :%d\n" +
-                "MULTIPLICAÇÃO: %d\n" , this.soma(), this.subtracao(), this.multiplicacao());
+
+    public String toString(int op) {
+
+        if(op == 1){
+            return "Soma: "+this.soma();
+        }
+        else if(op == 2){
+            return  "Subtração: "+this.subtracao();
+        }
+        else  if(op == 3){
+            return  "Multiplição: "+this.multiplicacao();
+        }
+        else {
+            return String.format("RESULTADO:\nSOMA: %d\n" +
+                    "SUBTRAÇÃO :%d\n" +
+                    "MULTIPLICAÇÃO: %d\n", this.soma(), this.subtracao(), this.multiplicacao());
+        }
     }
 }
 
--- src/Exercicios/Objeto/ATP30/Main.java
+++ src/Exercicios/Objeto/ATP30/Main.java
@@ -3,57 +3,99 @@ package Exercicios.Objeto.ATP30;
 import java.util.Scanner;
 
 public class Main {
+    static Calculadora cal = new Calculadora();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Calculadora cal = new Calculadora();
-        Scanner sc = new Scanner(System.in);
-        int num1 ;
+        int op = 0 ;
+        int num1;
         int num2 = 0;
-        int antigoNumero2 = 0;
-        int novoNumero2 = 0 ;
         boolean valido = true;
-
+        int antigoNumero2 = 0 ;
         do{
-            try {
-                System.out.println("Digite o 1° número:");
-                num1 = Integer.parseInt(sc.nextLine());
-                cal.setNumero1(num1);
-                valido = true;
+            System.out.println("\n=============================Menu=========================\n");
+            do{
+                try {
+                    System.out.println("Digite o 1° número:");
+                    num1 = Integer.parseInt(sc.nextLine());
+                    cal.setNumero1(num1);
+                    valido = true;
 
-                System.out.println("Digite o 2° número:");
-                num2 = Integer.parseInt(sc.nextLine());
-                cal.setNumero2(num2);
-                antigoNumero2 = num2;
-
-            }catch (Exception e){
-                System.out.println("Somente Números inteiros!");
-                valido = false;
-            }
-        }while (!valido);
-
-
-
-        try{
-            double resultado1  = cal.divisao(antigoNumero2);
-            System.out.println(cal.toString());
-            System.out.printf("Divisao: %.2f\n",resultado1);
-        }catch (Exception e){
-            do {
-                System.out.println("Não é possível fazer divisão por zero.");
-                try{
-                    System.out.println("Digite o 2° número novamente:");
+                    System.out.println("Digite o 2° número:");
                     num2 = Integer.parseInt(sc.nextLine());
-                    novoNumero2 = num2;
-                }catch (Exception ex){
-                    System.out.println("Somente número inteiros!");
+                    cal.setNumero2(num2);
+                    antigoNumero2 = num2;
+
+                } catch (Exception e) {
+                    System.out.println("Somente Números inteiros!");
+                    valido = false;
                 }
 
-            }while (num2 == 0  );
+            } while (!valido );
+            op = menu();
+            cases(op,num2,antigoNumero2);
+        }while (op != 5);
 
-            double resultado2 = cal.divisao(novoNumero2);
-            System.out.print(cal.toString());
-            System.out.printf("DIVISÃO: %.2f\n",resultado2);
+     }
 
+     static int menu(){
+        int op = 0;
+        try {
+            System.out.println("1 - SOMA\n2 - SUBTRAÇÃO\n3 - MULTIPLICAÇÃO\n4 - Divisão\n5 - Sair\n");
+            System.out.print("Digite uma opção:");
+            op = Integer.parseInt(sc.nextLine());
+        }catch (Exception e){
+            System.out.println("Somente numeros inteiros ");
         }
+         return op;
+     }
+
+     static void cases(int op,int num2 ,int antigoNumero2){
+            switch (op) {
+                case 1:
+                    System.out.println(cal.toString(1));
+                    break;
+                case 2:
+                    System.out.println(cal.toString(2));
+                    break;
+                case 3:
+                    System.out.println(cal.toString(3));
+                    break;
+
+                case 4:
+                    int novoNumero2 = 0;
+                    try{
+                        double resultado1  = cal.divisao(antigoNumero2);
+                        System.out.printf("Divisao: %.2f\n",resultado1);
+                    }catch (Exception e){
+
+                        do {
+                            System.out.println("Não é possível fazer divisão por zero.");
+                            try{
+                                System.out.println("Digite o 2° número novamente:");
+                                num2 = Integer.parseInt(sc.nextLine());
+                                novoNumero2 = num2;
+                            }catch (Exception ex){
+                                System.out.println("Somente número inteiros!");
+                            }
+
+                        }while (num2 == 0  );
+
+                        double resultado2 = cal.divisao(novoNumero2);
+                        System.out.printf("DIVISÃO: %.2f\n",resultado2);
+
+                    }
+                    break;
+                case 5:
+                    System.out.println("Até mais!");
+                    break;
+                default:
+                    System.out.println("Nâo existe essa opção!tente novamente");
+                    break;
+
+            }
+     }
+
+
 
     }
-}
